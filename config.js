dd8f64a34e517931a6cb75e1d5abde6c8f548a73
Export Team <yutthana@vanachai.com>
2026-06-15 14:41:34 +0700

    add: google oauth config

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  CLIENT_ID: "77396067327-8k80ehk85kne4buitm03l15qmf4cdn49.apps.googleusercontent.com",
+  SHEETS_API: "https://script.google.com/a/macros/vanachai.com/s/AKfycbyCaKlsKm0Cdmh7xJZt6RPV-2dOj8girN59E8ZeyZV0JJFg8J8x0vGjhSbxAbDdgYlFGg/exec",
+  DOMAIN: "vanachai.com"
+};
