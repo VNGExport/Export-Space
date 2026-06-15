22b30ac770f135f1398946dd37b217e8bfc6657a
Export Team <yutthana@vanachai.com>
2026-06-15 17:02:14 +0700

    config update Apps Script URL v3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   CLIENT_ID: "77396067327-8k80ehk85kne4buitm03l15qmf4cdn49.apps.googleusercontent.com",
-  SHEETS_API: "https://script.google.com/macros/s/AKfycbydbvX3gQGFMg-nPHSbhFAkJTpTS6IwE0G5h6sVH913wXszqVB33Hp-vDr_0TvILYGf6Q/exec",
+  SHEETS_API: "https://script.google.com/macros/s/AKfycbyXZLuuSFvOA0zhJkLWTw59BRQElhl4hEL1kA8hyn4y-nc47vHE2X5lm8XpHL8aY-Icsw/exec",
   DOMAIN: "vanachai.com"
 };
