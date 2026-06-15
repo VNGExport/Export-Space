425488820217754c2682ed31f104e273d7a85bb1
Export Team <yutthana@vanachai.com>
2026-06-15 15:42:43 +0700

    feat: connect Google Login + Sheets API

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   CLIENT_ID: "77396067327-8k80ehk85kne4buitm03l15qmf4cdn49.apps.googleusercontent.com",
-  SHEETS_API: "https://script.google.com/macros/s/AKfycbyOVLP70eaS7iMJHM0i62QFsOcEXwDX5JYpmCGjvzEg77UqFECNEb2Bm12CFAlvxIhdyw/exec",
+  SHEETS_API: "https://script.google.com/macros/s/AKfycbwbvGS8OqBaMb9Nx-pTPNV0rAJDap2x3m00PKW7XC7mc96H5eonOpR9UbWgMwHMNqgdiQ/exec",
   DOMAIN: "vanachai.com"
 };
